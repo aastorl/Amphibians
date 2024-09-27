ee64dfa8594a10c0e1e2ee5e954a1f59b6c91b03
aastorl <aastorl79@gmail.com>
2024-09-27 19:34:53 -0300

    Cambios en el Build Gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,5 +27,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.6.1" apply false
     id("com.android.library") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
